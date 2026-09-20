a63a1ac7b72a727dee1a9727e8ce4f0a3d17d21d
Larissa <arq.larissalara@gmail.com>
2026-09-20 15:15:46 -0300

    test: ajusta testes de Aged Brie, Normal e Sulfuras para arquitetura refatorada

--- tests/test_aged_brie.py
+++ tests/test_aged_brie.py
@@ -1,31 +1,26 @@
-from gilded_rose import GildedRose, Item
+from gilded_rose.item import Item
+from gilded_rose.updaters.aged_brie_updater import AtualizadorAgedBrie
 
 
-def test_aged_brie_increases_quality_by_one_before_sell_in():
-    item = Item(name="Aged Brie", sell_in=2, quality=0)
-    gilded_rose = GildedRose([item])
-    gilded_rose.att()
-    assert item.sell_in == 1
-    assert item.quality == 1
+def test_aged_brie_increases_quality_by_one():
+    item = Item(name="Aged Brie", sell_in=10, quality=20)
+    atualizador = AtualizadorAgedBrie()
+    atualizador.atualizar(item)
+    assert item.sell_in == 9
+    assert item.quality == 21
 
 
 def test_aged_brie_increases_quality_twice_as_fast_after_sell_in():
-    item = Item(name="Aged Brie", sell_in=0, quality=10)
-    gilded_rose = GildedRose([item])
-    gilded_rose.att()
+    item = Item(name="Aged Brie", sell_in=0, quality=20)
+    atualizador = AtualizadorAgedBrie()
+    atualizador.atualizar(item)
     assert item.sell_in == -1
-    assert item.quality == 12
+    assert item.quality == 22
 
 
 def test_aged_brie_quality_never_exceeds_fifty():
     item = Item(name="Aged Brie", sell_in=5, quality=50)
-    gilded_rose = GildedRose([item])
-    gilded_rose.att()
-    assert item.quality == 50
-
-
-def test_aged_brie_quality_caps_at_fifty_after_sell_in():
-    item = Item(name="Aged Brie", sell_in=-1, quality=49)
-    gilded_rose = GildedRose([item])
-    gilded_rose.att()
+    atualizador = AtualizadorAgedBrie()
+    atualizador.atualizar(item)
+    assert item.sell_in == 4
     assert item.quality == 50
--- tests/test_normal.py
+++ tests/test_normal.py
@@ -1,31 +1,26 @@
-from gilded_rose import GildedRose, Item
+from gilded_rose.item import Item
+from gilded_rose.updaters.normal_updater import AtualizadorNormal
 
 
 def test_normal_item_decreases_sell_in_and_quality_by_one():
     item = Item(name="+5 Dexterity Vest", sell_in=10, quality=20)
-    gilded_rose = GildedRose([item])
-    gilded_rose.att()
+    atualizador = AtualizadorNormal()
+    atualizador.atualizar(item)
     assert item.sell_in == 9
     assert item.quality == 19
 
 
 def test_normal_item_quality_degrades_twice_as_fast_after_sell_in():
     item = Item(name="+5 Dexterity Vest", sell_in=0, quality=20)
-    gilded_rose = GildedRose([item])
-    gilded_rose.att()
+    atualizador = AtualizadorNormal()
+    atualizador.atualizar(item)
     assert item.sell_in == -1
     assert item.quality == 18
 
 
-def test_normal_item_quality_is_never_negative():
+def test_normal_item_quality_never_negative():
     item = Item(name="+5 Dexterity Vest", sell_in=5, quality=0)
-    gilded_rose = GildedRose([item])
-    gilded_rose.att()
-    assert item.quality == 0
-
-
-def test_normal_item_quality_never_negative_after_sell_in():
-    item = Item(name="+5 Dexterity Vest", sell_in=-1, quality=1)
-    gilded_rose = GildedRose([item])
-    gilded_rose.att()
+    atualizador = AtualizadorNormal()
+    atualizador.atualizar(item)
+    assert item.sell_in == 4
     assert item.quality == 0
